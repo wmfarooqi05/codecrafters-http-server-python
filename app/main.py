809fd52e2bfc254e1326f835bc410366b15acf8e
Waleed Farooqi <wmfarooqi05@gmail.com>
2024-06-22 03:59:51 +0500

    Extract URL path #IH0

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 import socket
 
+OK_RESP = b"HTTP/1.1 200 OK\r\n\r\n"
+ERROR_RESP = b"HTTP/1.1 404 Not Found\r\n\r\n"
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -7,7 +11,24 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
+    while True:
+        connection, address = server_socket.accept()
+        data = connection.recv(1024).decode("utf-8").split('\r\n')
+
+        if not data:
+            break
+
+        data = data[0].split(' ')[1]
+
+        if data == "/":
+            connection.send(OK_RESP)
+        else:
+            connection.send(ERROR_RESP)
+
+        # Closing the connection
+        connection.close()
+
 
 if __name__ == "__main__":
     main()
