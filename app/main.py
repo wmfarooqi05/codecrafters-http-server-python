e941a22b89f0b50d4855ea1094d1b27a4b3ea91d
Waleed Farooqi <wmfarooqi05@gmail.com>
2024-06-22 02:51:23 +0500

    Respond with 200 #IA4

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import socket
 
-
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -8,8 +7,7 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     main()
