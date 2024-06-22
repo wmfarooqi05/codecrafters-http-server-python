26ed143ed7fde306ea7cc3e4d4a19d85a8a53bf0
Waleed Farooqi <wmfarooqi05@gmail.com>
2024-06-22 13:28:33 +0500

    Read request body #QV8

--- app/main.py
+++ app/main.py
@@ -17,13 +17,19 @@ def get_streaming_header(string):
     )
 
 
+def create_directory(directory_path):
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+        print(f"Directory '{directory_path}' created.")
+
+
 def parse_headers(data):
     headers = {}
     lines = data.split('\r\n')
     for line in lines:
         if ': ' in line:
             key, value = line.split(': ', 1)
-            headers[key] = value
+            headers[f"{key}".lower()] = value
     return headers
 
 
@@ -39,34 +45,51 @@ def main():
         connection, address = server_socket.accept()
         request_data = connection.recv(1024).decode("utf-8")
 
-        headers = parse_headers(request_data)
+        header_data, body = request_data.split('\r\n\r\n', 1)
+        headers = parse_headers(header_data)
 
         data = request_data.split('\r\n')
         if not data:
             break
 
+        request_method = data[0].split(' ')[0]
         path = data[0].split(' ')[1]
 
-        if path == "/":
-            connection.send(OK_RESP)
-        elif path.startswith("/echo"):
-            request_str = path.split("/echo/")[1]
-            connection.sendall(get_content_header(request_str))
-        elif path.startswith("/user-agent"):
-            user_agent = headers['User-Agent']
-            connection.sendall(get_content_header(user_agent))
-        elif path.startswith('/files/') and sys.argv[1] == '--directory':
-            file_name = path.replace('/files/', '')
-            file_path = os.path.join(sys.argv[2], file_name)
-            if os.path.exists(file_path):
-                with open(file_path, 'r') as file:
-                    file_content = file.read()
-                    connection.send(get_streaming_header(file_content))
+        if request_method.upper() == "GET":
+            if path == "/":
+                connection.send(OK_RESP)
+            elif path.startswith("/echo"):
+                request_str = path.split("/echo/")[1]
+                connection.sendall(get_content_header(request_str))
+            elif path.startswith("/user-agent"):
+                user_agent = headers['user-agent']
+                connection.sendall(get_content_header(user_agent))
+            elif path.startswith('/files/') and sys.argv[1] == '--directory':
+                file_name = path.replace('/files/', '')
+                file_path = os.path.join(sys.argv[2], file_name)
+                if os.path.exists(file_path):
+                    with open(file_path, 'r') as file:
+                        file_content = file.read()
+                        connection.send(get_streaming_header(file_content))
+                else:
+                    connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
             else:
-                connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                connection.send(ERROR_RESP)
+        elif request_method.upper() == "POST":
+            if path.startswith('/files/') and \
+                    sys.argv[1] == '--directory' and \
+                    headers['content-type'] == "application/octet-stream" and \
+                    headers['content-length'] is not None:
+                file_name = path.replace('/files/', '')
+                full_path = os.path.join(sys.argv[2], file_name)
+                create_directory("".join(full_path.split('/')[:-1]))
+
+                with open(full_path, 'w') as file:
+                    file.write(body)
+                    connection.send(b'HTTP/1.1 201 Created\r\n\r\n')
+
         else:
             connection.send(ERROR_RESP)
-
         # Closing the connection
         connection.close()
 
