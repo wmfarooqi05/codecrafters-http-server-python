57d6763f38b8d3e76943986f4be508b5372fdf34
Waleed Farooqi <wmfarooqi05@gmail.com>
2024-06-22 12:58:12 +0500

    Return a file #AP6

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
+import os.path
 import socket
+import sys
 
 OK_RESP = b"HTTP/1.1 200 OK\r\n\r\n"
 ERROR_RESP = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
 
 def get_content_header(string):
-    return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}"
+    return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
+
+
+def get_streaming_header(string):
+    return f"HTTP/1.1 200 OK\r\nContent-Type: octet-stream\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
 
 
 def parse_headers(data):
@@ -42,10 +48,19 @@ def main():
             connection.send(OK_RESP)
         elif path.startswith("/echo"):
             request_str = path.split("/echo/")[1]
-            connection.sendall(get_content_header(request_str).encode())
+            connection.sendall(get_content_header(request_str))
         elif path.startswith("/user-agent"):
             user_agent = headers['User-Agent']
-            connection.sendall(get_content_header(user_agent).encode())
+            connection.sendall(get_content_header(user_agent))
+        elif path.startswith('/files/') and sys.argv[1] == '--directory':
+            file_name = path.replace('/files/', '')
+            file_path = os.path.join(sys.argv[2], file_name)
+            if os.path.exists(file_path):
+                with open(file_path, 'r') as file:
+                    file_content = file.read()
+                    connection.send(get_streaming_header(file_content))
+            else:
+                connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
         else:
             connection.send(ERROR_RESP)
 
